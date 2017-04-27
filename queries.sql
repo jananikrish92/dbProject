f765fd49ad1ad500d1921aef981191dd1d693d48
ananyasuraj07 <asuraj@cs.umass.edu>
2017-04-27 12:53:58 -0400

    Nobel lauretes

--- queries.sql
+++ queries.sql
@@ -1,40 +1,44 @@
-—-create database nobel_l;
+
 --Now run dbproj.py
 select * from nobel limit 1;
---Create table to store laureates from 1900-1909
-create table name_1900(id INTEGER, name TEXT,category TEXT,year TEXT);
---insert values
-insert into name_1900(id,name,category,year)(select index,full_name,category,year from nobel where year>=1900 and year<=1909);
---Create table to store laureates from 1910-1919
-create table name_1910(id INTEGER, name TEXT,category TEXT,year TEXT);
---insert values
-insert into name_1910(id,name,category,year)(select index,full_name,category,year from nobel where year>=1910 and year<=1919);
+
+create table name_1900_1909(name TEXT,category TEXT,year TEXT);
+insert into name_1900_1909(name,category,year)(select full_name,category,year from nobel where year>=1900 and year<=1909);
+
+create table name_1910_1919(name TEXT,category TEXT,year TEXT);
+insert into name_1910_1919(name,category,year)(select full_name,category,year from nobel where year>=1910 and year<=1919);
+
+create table name_1920_1929(name TEXT,category TEXT,year TEXT);
+insert into name_1920_1929(name,category,year)(select full_name,category,year from nobel where year>=1920 and year<=1929);
+
+create table name_1930_1939(name TEXT,category TEXT,year TEXT);
+insert into name_1930_1939(name,category,year)(select full_name,category,year from nobel where year>=1930 and year<=1939);
+
+create table name_1940_1949(name TEXT,category TEXT,year TEXT);
+insert into name_1940_1949(name,category,year)(select full_name,category,year from nobel where year>=1940 and year<=1949);
+
+create table name_1950_1959(name TEXT,category TEXT,year TEXT);
+insert into name_1950_1959(name,category,year)(select full_name,category,year from nobel where year>=1950 and year<=1959);
+
+create table name_1960_1969(name TEXT,category TEXT,year TEXT);
+insert into name_1960_1969(name,category,year)(select full_name,category,year from nobel where year>=1960 and year<=1969);
 
 
+create table nobel_prize(name TEXT,prize TEXT);
+insert into nobel_prize(select full_name,prize from nobel);
 
-create table t1(id integer,name text,category text,year text);
-insert into t1( select index,full_name,category,year from nobel);
+create table nobel_lauretes_country(name TEXT, birth_country TEXT, category TEXT);
+insert into nobel_lauretes_country(select full_name,birth_country,category from nobel);
 
+create table nobel_lauretes_city(name TEXT,birth_city TEXT,year TEXT, category TEXT);
+insert into nobel_lauretes_city(select full_name,birth_city,year,category from nobel);
 
-create table t2(id integer,prize text,motivation text,
+create table nobel_lauretes_data(id integer,prize text,motivation text,
 	prize_share text,laureate_id integer, laureate_type text,full_name text,birth_date text,
  birth_city text,birth_country text,sex text,organization_name text,organization_city text,organization_country text,
 death_date text, death_city text,death_country text);
 
-insert into t2(select index,prize ,motivation ,prize_share ,laureate_id , laureate_type ,full_name ,birth_date ,
+ 
+insert into nobel_lauretes_data(select index,prize ,motivation ,prize_share ,laureate_id , laureate_type ,full_name ,birth_date ,
  birth_city ,birth_country ,sex ,organization_name ,organization_city ,organization_country ,
-death_date , death_city ,death_country from nobel)
-
-
-
-
-create table t3(id integer ,prize text,motivation text,prize_share text,laureate_id integer ,laureate_type text,full_name text,birth_date text);
-insert into t3( select index,prize,motivation,prize_share,laureate_id,laureate_type,full_name,birth_date  from nobel);
-
-
-create table t4(id integer, laureate_type  text, full_name text, birth_date text, birth_city text, birth_country text, sex text, organization_name text, organization_city text, organization_country text, death_date text, death_city text, death_country text );
-insert into t4( select index,laureate_type, full_name, birth_date, birth_city, birth_country, sex, organization_name, organization_city, organization_country, death_date, death_city, death_country from nobel);
-
-create table temp(column_name text, table_name text);
-insert into temp(column_name,table_name)(select column_name,table_name from information_schema.columns where table_name in (select table_name from information_schema.tables where table_schema='public' and table_name!='nobel' and table_name!='temp'));
-create table index_table(value text, column_name text, table_name text);
+death_date , death_city ,death_country from nobel);
